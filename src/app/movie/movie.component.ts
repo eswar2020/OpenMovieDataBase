1c110cdcf18821b1e3e1f0f6b0a19378a16e9abc
eswar2020 <mullapudieswar20@gmail.com>
2020-05-01 22:38:21 +0530

    added dependency Injection

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OmdbService } from '../omdb.service';
 
 @Component({
   selector: 'app-movie',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.component.css']
 })
 export class MovieComponent implements OnInit {
-
-  constructor() { }
+movie;
+  constructor(private omdb:OmdbService) { }
 
   ngOnInit() {
   }
 
+    getMovieData() {
+     this.omdb.getMovie().subscribe(
+       response => this.movie = response
+     )
+    }
 }
--- src/app/omdb.service.ts
+++ src/app/omdb.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  getMovie() {
+  return this.http.get("http://www.omdbapi.com/?t=[yourkey]");
+  }
 }
